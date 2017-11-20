72ecdf312c670b6e8fb6d2cf702344a67cccc7ed
ashenafieshete <ashenafi_eshete@yahoo.com>
2017-11-20 13:18:55 -0800

    Seleprojchange

---
+++ Desktop2/QeDemo/src/ashuDemo/Seleniumpro.java
@@ -0,0 +1,21 @@
+package ashuDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Seleniumpro {
+
+	public void main(String[] args) {
+		
+		// Users/ashenafieshete/Desktop2/QeDemo/lib/chromedriver
+		
+    /*System.setProperty("WebDriver.Firefox","Users/ashenafieshete/Desktop2/QeDemo/lib/FirefoxDriver");
+    WebDriver Driver = new FirefoxDriver();
+    driver.get("http://www.facebook.com");
+    System.out.println(Driver.getTitle());
+    System.out.println("I got you the title");
+    driver.quit();
+    	System.out.println("Browser closed");*/
+	}
+
+}
