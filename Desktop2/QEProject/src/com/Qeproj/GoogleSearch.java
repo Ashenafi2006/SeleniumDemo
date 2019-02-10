1844064a39bfdaa708a0bd1cb8e3586e545c2506
ashenafieshete <ashenafi_eshete@yahoo.com>
2019-02-09 19:40:15 -0800

    update

---
+++ Desktop2/QEProject/src/com/Qeproj/GoogleSearch.java
@@ -0,0 +1,31 @@
+package com.Qeproj;
+import java.lang.util;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Assert;
+
+import org.openqa.selenium.WebElement;
+
+
+public class GoogleSearch {
+	
+	public static void main(String [] args) {
+		/*Key is methods that chromedriver recognize, 
+		 and the location of chromedriver on your computer
+		 */
+		        System.setProperty("webdriver.chrome.driver", "libs/chromedriver");
+		        WebDriver driver = new ChromeDriver();
+		        driver.get("http://www.google.com");
+		        driver.findElement(By.name("q")).click();
+		        driver.findElement(By.name("q")).sendKeys("Barck Obama");
+		        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		        //driver.findElement(By.linkText("Images")).click();
+		        driver.findElement(By.xpath("//*[@id='rso']/div[4]/div/div[1]/div/div/div[1]/a/h3")).click();
+		    
+		    //XPath: //input[contains(@id, '_name')]
+		    
+	}
+		}
